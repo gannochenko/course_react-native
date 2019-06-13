16cb8b40facc8878f9e9e574c96109f55540634b
Sergei <gannochenko.sv@gmail.com>
2019-06-13 18:05:19 +0200

    eslint

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, WebView } from 'react-native';
+// import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+    // https://kmagiera.github.io/react-native-gesture-handler/docs/component-swipeable.html
+    render() {
+        return <Text>Some text</Text>;
+
+        // return (
+        //     <Swipeable
+        //         style={{ flex: 1 }}
+        //         renderRightActions={(progress, dragX) => {
+        //             return <Text>"I am here!"</Text>;
+        //         }}
+        //     >
+        //         <Text>
+        //             <View style={{ flex: 1 }}>
+        //                 <View
+        //                     style={{ flex: 1, backgroundColor: 'powderblue' }}
+        //                 />
+        //                 <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+        //                 <View
+        //                     style={{ flex: 3, backgroundColor: 'steelblue' }}
+        //                 />
+        //             </View>
+        //         </Text>
+        //     </Swipeable>
+        // );
+
+        //return <WebView source={{ uri: 'http://192.168.178.23:3000/' }} />;
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+//     hello: {
+//         flex: 1,
+//     },
+// });
+
+export default App;
